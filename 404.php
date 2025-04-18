d17474e7edb0b617021a04fc6e651f540a3175bd
parrot-xxiv <parrot-xxiv@users.noreply.github.com>
2025-04-18 20:58:33 +0800

    modify based on _s theme

--- 404.php
+++ 404.php
@@ -1,1 +1,29 @@
-<div>404 Not found</div>
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Custom_Theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<section class="error-404 not-found">
+			<header class="page-header">
+				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'custom-theme' ); ?></h1>
+			</header><!-- .page-header -->
+
+			<div class="page-content">
+				<p><?php esc_html_e( 'It looks like nothing was found at this location. ', 'custom-theme' ); ?></p>
+				<a href="<?php get_home_url();?>">Back to HomePage</a>
+			</div><!-- .page-content -->
+		</section><!-- .error-404 -->
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
